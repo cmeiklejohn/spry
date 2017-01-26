cebfee38a80468c9e3d37fa90fee54c31b3b8163
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2017-01-26 11:34:22 +0100

    Add test suite and reduction.

--- src/spry_backend.erl
+++ src/spry_backend.erl
@@ -70,9 +70,16 @@ handle_call({reduce, ObjectId,
     lager:info("Reducing ~p with latency bound of ~p ms",
                [ObjectId, Milliseconds]),
 
+    %% Retrieve object origin.
+    [{_, #object{origin=Origin}}] = ets:lookup(?MODULE, ObjectId),
+
     %% Set timer to retrieve the value from the other node.
     timer:send_after(Milliseconds,
                      {reduce_timeout, From, Milliseconds, ObjectId}),
+
+    %% Issue request for the value.
+    Self = node(),
+    gen_server:cast({?MODULE, Origin}, {refresh, Self, ObjectId}),
 
     {noreply, State};
 
@@ -81,6 +88,26 @@ handle_call(Msg, _From, State) ->
     {reply, ok, State}.
 
 %% @private
+handle_cast({refresh, From, ObjectId}, State) ->
+    Self = node(),
+    lager:info("Received refresh from ~p", [Self]),
+
+    %% Retrieve object origin.
+    [{ObjectId, Object}] = ets:lookup(?MODULE, ObjectId),
+
+    %% Reply.
+    gen_server:cast({?MODULE, From}, {value, ObjectId, Object}),
+
+    {noreply, State};
+
+handle_cast({value, ObjectId, Object}, State) ->
+    lager:info("Refreshed value received: ~p", [ObjectId]),
+
+    %% Store new value.
+    true = ets:insert(?MODULE, {ObjectId, Object}),
+
+    {noreply, State};
+
 handle_cast(Msg, State) ->
     _ = lager:warning("Unhandled messages: ~p", [Msg]),
     {noreply, State}.
@@ -92,6 +119,8 @@ handle_info({reduce_timeout, From, Milliseconds, ObjectId}, State) ->
 
     %% Retrieve current value.
     [{_, #object{value=Value}}] = ets:lookup(?MODULE, ObjectId),
+
+    lager:info("Returning value is: ~p", [Value]),
 
     %% Respond back to the waiting clients.
     gen_server:reply(From, {ok, Value}),
---
+++ test/spry_SUITE.erl
@@ -0,0 +1,37 @@
+-module(spry_SUITE).
+-author("Christopher S. Meiklejohn <christopher.meiklejohn@gmail.com>").
+
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+-export([all/0,
+         groups/0,
+         % init_per_suite/1,
+         % end_per_suite/1,
+         % init_per_group/2,
+         % end_per_group/2,
+         init_per_testcase/2,
+         end_per_testcase/2
+        ]).
+
+-export([basic/1]).
+
+all() ->
+    [basic].
+
+groups() ->
+    [].
+
+init_per_testcase(_Name, Config) ->
+    {ok, _Apps} = application:ensure_all_started(spry),
+    Config.
+
+end_per_testcase(_Name, _Config) ->
+    ok.
+
+basic(_Config) ->
+    Node = node(),
+    ObjectId = <<"123">>,
+    Constraints = [{latency, 100}],
+    ok = spry_backend:declare(ObjectId, Node),
+    {ok, undefined} = spry_backend:reduce(ObjectId, Constraints).
